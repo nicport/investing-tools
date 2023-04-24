e26cd832ea9b1e39d8296be12a8f35f16308f9a5
nicport <nicporter1013@gmail.com>
2023-04-24 18:04:00 -0400

    added better error handling

--- get_ticker_info.py
+++ get_ticker_info.py
@@ -1,14 +1,26 @@
 import csv
 import yfinance as yf
 
+
+filename_csv = 'stock_tickers.csv'
+''' 
+  optional: 
+    if all the stocks in the csv are from the same exchange this will help
+    reduce ambiguity. In this case I am using .TO for the Toronto Stock Exchange
+'''
+exchange = '.TO'
+
 if __name__ == '__main__':
-    with open('stock_tickers.csv') as data:
-      csvreader = csv.reader(data, delimiter='\n')
-      for row in csvreader:
-        print('Getting information for ticker ' + row[0] + '......')
-        try:
-          stock = yf.Ticker(row[0])
-          print(stock.info)
-        except Exception as e:
-           print(e)
-    data.close()
+    try:
+      with open(filename_csv) as data:
+        csvreader = csv.reader(data, delimiter='\n')
+        for row in csvreader:
+          print('Getting information for ticker ' + row[0] + '......')
+          try:
+            stock = yf.Ticker(row[0] + exchange)
+            print(stock.info)
+          except Exception as e:
+            print(f'Error getting information for ticker {row[0]} : {e}')
+      data.close()
+    except Exception as e:
+      print(f'Error opening file {filename_csv} : {e}')
